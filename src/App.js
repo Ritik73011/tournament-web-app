b4f9ef07545a4eb42e0a56ae29ef5961db32ed1f
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-15 15:05:52 +0530

    routing added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
-
+import {Routes,Route} from 'react-router'
+import Home from './pages/Home';
+import Navbar from './components/Navbar/Navbar';
 function App() {
   return (
     <div className="App">
-      
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+      </Routes>
     </div>
   );
 }
