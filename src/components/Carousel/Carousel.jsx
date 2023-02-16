262f536e4790fac351a36e630f37a749d8522554
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-16 19:29:53 +0530

    automatic carousel and image changed

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -2,22 +2,22 @@ import { Box } from "@mui/material";
 import Carousel from "nuka-carousel";
 const Carousels = () => {
   const arr = [
-    "https://www.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg",
-    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg",
-    "https://images.ctfassets.net/hrltx12pl8hq/a2hkMAaruSQ8haQZ4rBL9/8ff4a6f289b9ca3f4e6474f29793a74a/nature-image-for-website.jpg?fit=fill&w=480&h=320",
-    "https://cdn.pixabay.com/photo/2018/01/12/10/19/fantasy-3077928__340.jpg",
+    "https://wallpapers.com/images/featured/53iv6fl796p3l763.jpg",
+    "https://firebasestorage.googleapis.com/v0/b/agumentik-company.appspot.com/o/unnamed%20(1).png?alt=media&token=2b111131-eecc-4c2d-be5b-b27ebaa4b977",
+    "https://images.hindustantimes.com/tech/img/2022/06/23/1600x900/54f31449f5f91cf0cc223cc635cd5952jpg_1655955051259_1655955067513.jpeg",
+    "https://4kwallpapers.com/images/wallpapers/bgmi-jinx-pubg-arcane-crossover-2022-games-3440x1440-7316.jpg",
   ];
 
   return (
     <Box>
-      <Carousel>
+      <Carousel autoplayInterval={3000} autoplay={true}>
         {arr.map((ele, indx) => {
           return (
             <img
               key={indx + 1}
               src={ele}
               alt="img"
-              style={{ width: "100%", height: "400px" }}
+              style={{ width: "100%", height: "100%",maxHeight:"600px" }}
             />
           );
         })}
