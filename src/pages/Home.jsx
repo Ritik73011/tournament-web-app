0469edd82d677f6e1452be533f47ab22401b61a8
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-17 16:07:26 +0530

    media query added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,21 @@
+import { Box,useMediaQuery } from "@mui/material"
 import Bgmi from "../components/BGMI/Bgmi"
 import Carousels from "../components/Carousel/Carousel"
 
 const Home = () => {
+  const media = useMediaQuery("(max-width:900px)");
+  const media2 = useMediaQuery("(max-width:600px)");
+  const style900 = {
+    marginTop:media?"64px":"68.5px",
+  }
+  const style600 ={
+    marginTop:"56px"
+  }
   return (
-    <div>
+    <Box sx={media2?style600:style900}>
       <Carousels/>
       <Bgmi/>
-    </div>
+    </Box>
   )
 }
 
