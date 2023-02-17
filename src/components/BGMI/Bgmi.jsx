2ac8abda7cff0a4e9cd562232abe3bc44feb05d9
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-17 16:08:20 +0530

    bgmi

--- src/components/BGMI/Bgmi.jsx
+++ src/components/BGMI/Bgmi.jsx
@@ -1,8 +1,9 @@
-import {Box} from '@mui/material'
+import {Box, Typography} from '@mui/material'
 const Bgmi = () => {
   return (
-    <Box>
-        
+    <Box sx={{textAlign:"left",margin:"8px 0"}}>
+        <Typography variant='h4'>Battle Ground Mobile India</Typography>
+        <Box sx={{width:"100%",height:"320px",background:"pink"}}></Box>
     </Box>
   )
 }
