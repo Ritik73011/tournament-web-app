8d70efd2b4e6490cd28cdad017897feaa61c9dd8
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-19 13:21:42 +0530

    more key added in array

--- src/components/BGMI/Bgmi.jsx
+++ src/components/BGMI/Bgmi.jsx
@@ -2,19 +2,96 @@ import { Box, Typography } from "@mui/material";
 import BgmiCard from "../BgmiCard/BgmiCard";
 const Bgmi = () => {
   const arr = [
-    { id: 1, title: "Match 1",map:"Erangle" },
-    { id: 2, title: "Match 2",map:"Livik" },
-    { id: 3, title: "Match 3",map:"Miramar" },
-    { id: 4, title: "Match 4",map:"Erangle" },
-    { id: 5, title: "Match 5",map:"Sanhok" },
+    {
+      id: 1,
+      title: "Match 1",
+      seat: "100",
+      joined: "42",
+      type: "Squad",
+      version: "TPP",
+      perKill: "12",
+      entryFee: "20",
+      map: "Erangle",
+    },
+    {
+      id: 2,
+      title: "Match 2",
+      seat: "100",
+      joined: "0",
+      type: "Solo",
+      version: "TPP",
+      perKill: "12",
+      entryFee: "20",
+      map: "Livik",
+    },
+    {
+      id: 3,
+      title: "Match 3",
+      seat: "100",
+      joined: "11",
+      type: "Squad",
+      version: "TPP",
+      perKill: "12",
+      entryFee: "20",
+      map: "Miramar",
+    },
+    {
+      id: 4,
+      title: "Match 4",
+      seat: "100",
+      joined: "23",
+      type: "Squad",
+      version: "TPP",
+      perKill: "12",
+      entryFee: "20",
+      map: "Erangle",
+    },
+    {
+      id: 5,
+      title: "Match 5",
+      seat: "100",
+      joined: "96",
+      type: "Solo",
+      version: "TPP",
+      perKill: "12",
+      entryFee: "20",
+      map: "Sanhok",
+    },
   ];
   return (
     <Box sx={{ textAlign: "left", margin: "8px 0" }}>
       <Typography variant="h4">Battle Ground Mobile India</Typography>
       <Box sx={{ width: "100%", height: "320px", background: "pink" }}>
-        {arr.map(({ id, title,map }) => {
-          return <BgmiCard key={id} match={{ id, title,map }} />;
-        })}
+        {arr.map(
+          ({
+            id,
+            title,
+            map,
+            seat,
+            joined,
+            type,
+            version,
+            perKill,
+            entryFee,
+          }) => {
+            return (
+              <BgmiCard
+                key={id}
+                match={{
+                  id,
+                  title,
+                  map,
+                  seat,
+                  joined,
+                  type,
+                  version,
+                  perKill,
+                  entryFee,
+                }}
+              />
+            );
+          }
+        )}
       </Box>
     </Box>
   );
