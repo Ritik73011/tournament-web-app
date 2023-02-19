0bd0508fdfcaa28b7f3a2a198b22a4683e0a4b04
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-19 13:04:20 +0530

    match card added

--- src/components/BgmiCard/BgmiCard.jsx
+++ src/components/BgmiCard/BgmiCard.jsx
@@ -1,10 +1,17 @@
-import { Box, Typography } from "@mui/material";
+import { Card, Typography,CardContent } from "@mui/material";
+import { Box } from "@mui/system";
 const BgmiCard = ({ match }) => {
   return (
-    <Box>
-      <Typography>{match.id}</Typography>
-      <Typography variant="h5">{match.title}</Typography>
-    </Box>
+    <Card sx={{ minWidth:"300px",maxWidth:"400px",width:"100%" }}>
+    <CardContent>
+     <Box sx={{width:"100%",position:"relative"}}>
+        <img width={'100%'} src="https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/e97a75100099495.60427765e8dd2.jpg" alt="" />
+        <Typography sx={{color:"white",position:"absolute",top:4,left:6 }}>#id: {match.id}</Typography>
+        <Typography sx={{color:"white",position:"absolute",top:4,right:6 }}>{match.map}</Typography>
+     </Box>
+     <Typography variant="h6">{match.map} {match.title}</Typography>
+    </CardContent>
+  </Card>
   );
 };
 
