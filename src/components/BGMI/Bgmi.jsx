4c789ff8fc14bf2c1a2452c5efa704a6fbea5fc3
Ritik73011 <ritikkumarsingh730@gmail.com>
2023-02-19 13:55:21 +0530

    grid view

--- src/components/BGMI/Bgmi.jsx
+++ src/components/BGMI/Bgmi.jsx
@@ -5,8 +5,8 @@ const Bgmi = () => {
     {
       id: 1,
       title: "Match 1",
-      seat: "100",
-      joined: "42",
+      seat: 100,
+      joined: 42,
       type: "Squad",
       version: "TPP",
       perKill: "12",
@@ -16,8 +16,8 @@ const Bgmi = () => {
     {
       id: 2,
       title: "Match 2",
-      seat: "100",
-      joined: "0",
+      seat: 100,
+      joined: 0,
       type: "Solo",
       version: "TPP",
       perKill: "12",
@@ -27,8 +27,8 @@ const Bgmi = () => {
     {
       id: 3,
       title: "Match 3",
-      seat: "100",
-      joined: "11",
+      seat: 100,
+      joined: 11,
       type: "Squad",
       version: "TPP",
       perKill: "12",
@@ -38,8 +38,8 @@ const Bgmi = () => {
     {
       id: 4,
       title: "Match 4",
-      seat: "100",
-      joined: "23",
+      seat: 100,
+      joined: 23,
       type: "Squad",
       version: "TPP",
       perKill: "12",
@@ -49,8 +49,8 @@ const Bgmi = () => {
     {
       id: 5,
       title: "Match 5",
-      seat: "100",
-      joined: "96",
+      seat: 100,
+      joined: 96,
       type: "Solo",
       version: "TPP",
       perKill: "12",
@@ -61,37 +61,10 @@ const Bgmi = () => {
   return (
     <Box sx={{ textAlign: "left", margin: "8px 0" }}>
       <Typography variant="h4">Battle Ground Mobile India</Typography>
-      <Box sx={{ width: "100%", height: "320px", background: "pink" }}>
-        {arr.map(
-          ({
-            id,
-            title,
-            map,
-            seat,
-            joined,
-            type,
-            version,
-            perKill,
-            entryFee,
-          }) => {
-            return (
-              <BgmiCard
-                key={id}
-                match={{
-                  id,
-                  title,
-                  map,
-                  seat,
-                  joined,
-                  type,
-                  version,
-                  perKill,
-                  entryFee,
-                }}
-              />
-            );
-          }
-        )}
+      <Box sx={{ width: "100%",display:"grid",gap:"16px",gridTemplateColumns:"repeat(4,1fr)"}}>
+        {arr.map((ele) => {
+          return <BgmiCard key={ele.id} match={ele} />;
+        })}
       </Box>
     </Box>
   );
